0e5cd0ad7d8840d5530e0dcd7233c0493d97a5c1
leo <leo_9_t@hotmail.com>
2020-08-28 09:53:50 -0400

    ADDED https

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 require('dotenv').config()
+import { createServer } from "https"
+import { readFileSync } from "fs"
 import {app, bootstrap} from './app'
 import {AddressInfo} from 'net'
 
 bootstrap()
 .then(() => {
-    const server = app.listen(parseInt(process.env.PORT || '5000') || 5000, '0.0.0.0', () => {
-        const {port, address} = server.address() as AddressInfo;
-        console.log('Server listening on:','http://' + address + ':'+port);
-    });
+    const port = parseInt(process.env.PORT || '5000') || 5000
+    if (process.env.HTTPS) {
+        var options = {
+            key: readFileSync('./certs/server-key.pem'),
+            cert: readFileSync('./certs/server-cert.pem'),
+        };
+
+        var server = createServer(options, app).listen(port, function(){
+            console.log("Express server listening on port " + port);
+        });
+    } else {
+        const server = app.listen(port, '0.0.0.0', () => {
+            const {port, address} = server.address() as AddressInfo;
+            console.log('Server listening on:','http://' + address + ':'+port);
+        });
+    }
 })
